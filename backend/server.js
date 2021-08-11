3818cd6b9532883d5ba2595dcfb4e5999cc259b3
N Young <60322086+nai-young@users.noreply.github.com>
2021-08-11 19:39:20 +0100

    Updated server port

--- backend/server.js
+++ backend/server.js
@@ -43,7 +43,7 @@ app.use('/notes/', noteRouter)
 connectDB()
 
 // Server connection
-const port = 5000
+const port = process.env.PORT || 5000
 app.listen(port, () => {
   console.log(`Server running on port: ${port}`)
 })
